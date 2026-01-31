6ee8c8babb23824242096c6175531ff06ab82a00
BlackCoder56 <blackcoda56@gmail.com>
2026-01-31 21:31:37 +0300

    Modify the program in (b) above to: i) Accept and process scores for five (5) students using a while loop. ii) After all entries, display a summary showing how many students scored each grade (1 to 9).

--- src/physicgradingsystem/PhysicGradingSystem.java
+++ src/physicgradingsystem/PhysicGradingSystem.java
@@ -1,6 +1,5 @@
 package physicgradingsystem;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -11,77 +10,114 @@ public class PhysicGradingSystem {
 
     public static void main(String[] args) {
         
-        System.out.print("Enter a student’s score out of 100:");
-        try (Scanner user_input = new Scanner(System.in)) {
-            int entered_score = user_input.nextInt();
-
-           
+        int i = 1;
+//        int entered_score=0;
+        int entered_score1=0;
+        int entered_score2=0;
+        int entered_score3=0;
+        int entered_score4=0;
+        int entered_score5=0;
+        
+        Scanner user_input = new Scanner(System.in);
+            
+            while(i<=5){
+                
+//                entered_score = user_input.nextInt();
+                if(i==1){
+                    System.out.print("Enter 1st student’s score:");
+                    entered_score1 = user_input.nextInt();
+                }
+                if(i==2){
+                    System.out.print("Enter 2nd student’s score:");
+                    entered_score2 = user_input.nextInt();
+                }
+                if(i==3){
+                    System.out.print("Enter 3rd student’s score:");
+                    entered_score3 = user_input.nextInt();
+                }
+                if(i==4){
+                    System.out.print("Enter 4th student’s score:");
+                    entered_score4 = user_input.nextInt();
+                }
+                if(i==5){
+                    System.out.print("Enter 5th student’s score:");
+                    entered_score5 = user_input.nextInt();
+                    
+                }
+                i++;
+            }  
+            
+            displaySummary(entered_score1);
+            displaySummary(entered_score2);
+            displaySummary(entered_score3);
+            displaySummary(entered_score4);
+            displaySummary(entered_score5);
+            
+        
+    }
+//    method to display summary
+    public static void displaySummary(int entered_score){
             int grade;
             String remarks;
 
             if(entered_score >= 80 && entered_score <= 100){
                 grade = 1;
                 remarks = "D1";
-                System.out.println("The score:"+entered_score+" marks");
+                System.out.println("\nThe score:"+entered_score+"marks");
                 System.out.println("Grade:"+grade);
                 System.out.println("Remark:"+remarks);
             } else if (entered_score >= 75 && entered_score <= 79){
                 grade = 2;
                 remarks = "D2";
-                System.out.println("The score:"+entered_score+" marks");
+                System.out.println("\nThe score:"+entered_score+"marks");
                 System.out.println("Grade:"+grade);
                 System.out.println("Remark:"+remarks);
             } else if (entered_score >= 66 && entered_score <= 74){
                 grade = 3;
                 remarks = "C3";
-                System.out.println("The score:"+entered_score+" marks");
+                System.out.println("\nThe score:"+entered_score+"marks");
                 System.out.println("Grade:"+grade);
                 System.out.println("Remark:"+remarks);
             } else if (entered_score >= 60 && entered_score <= 65){
                 grade = 4;
                 remarks = "C4";
-                System.out.println("The score:"+entered_score+" marks");
+                System.out.println("\nThe score:"+entered_score+"marks");
                 System.out.println("Grade:"+grade);
                 System.out.println("Remark:"+remarks);
             } else if (entered_score >= 50 && entered_score <= 59){
                 grade = 5;
                 remarks = "C5";
-                System.out.println("The score:"+entered_score+" marks");
+                System.out.println("\nThe score:"+entered_score+"marks");
                 System.out.println("Grade:"+grade);
                 System.out.println("Remark:"+remarks);
             } else if (entered_score >= 45 && entered_score <= 49) {
                 grade = 6;
                 remarks = "C6";
-                System.out.println("The score:"+entered_score+" marks");
+                System.out.println("\nThe score:"+entered_score+"marks");
                 System.out.println("Grade:"+grade);
                 System.out.println("Remark:"+remarks);
             } else if (entered_score >= 35 && entered_score <= 44){
                 grade = 7;
                 remarks = "P7";
-                System.out.println("The score:"+entered_score+" marks");
+                System.out.println("\nThe score:"+entered_score+"marks");
                 System.out.println("Grade:"+grade);
                 System.out.println("Remark:"+remarks);
             } else if (entered_score >= 30 && entered_score <= 34){
                 grade = 8;
                 remarks = "P8";
-                System.out.println("The score:"+entered_score+" marks");
+                System.out.println("\nThe score:"+entered_score+"marks");
                 System.out.println("Grade:"+grade);
                 System.out.println("Remark:"+remarks);
             } else if (entered_score >= 0 && entered_score <= 29) {
                 grade = 9;
                 remarks = "F";
-                System.out.println("The score:"+entered_score+" marks");
+                System.out.println("\nThe score:"+entered_score+"marks");
                 System.out.println("Grade:"+grade);
                 System.out.println("Remark:"+remarks);
             } else{
                 System.out.println("Invalid input.");
             }
             
-        } catch (InputMismatchException e) {
-            System.out.println("Invalid input! Please enter a numeric value only.");
-        }
-        
-        
-    }
+            }
     
 }
